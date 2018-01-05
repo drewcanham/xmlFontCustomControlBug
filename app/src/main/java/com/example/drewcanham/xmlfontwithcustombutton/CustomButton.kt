43877c2fd96a9073b9639268118ee54e25a44ae8
drewcanham <drewc@iqmetrix.com>
2018-01-05 14:45:58 -0600

    recreating bug

---
+++ app/src/main/java/com/example/drewcanham/xmlfontwithcustombutton/CustomButton.kt
@@ -0,0 +1,17 @@
+package com.example.drewcanham.xmlfontwithcustombutton
+
+import android.content.Context
+import android.support.v7.widget.AppCompatButton
+import android.util.AttributeSet
+
+
+/**
+ * Created by Drew.Canham on 2017-12-15.
+ */
+class CustomButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
+                                                  defStyle: Int = R.attr.customButtonStyle) : AppCompatButton(context, attrs, defStyle) {
+
+    init {
+
+    }
+}
